d9493d3922eb7b2c27c9c8aa9029cd764513e0c8
allajka <allajka94@gmail.com>
2022-09-19 17:19:56 +0300

    first lectures

---
+++ lectures/exam001_Hello/hello.py
@@ -0,0 +1,9 @@
+name = 'Alla' 
+print('Hello ' + name + '!')
+print(type(name)) #комментарий
+
+print("Введите a")
+a = int(input())
+print("Введите b")
+b = int(input())
+print(a, '+', b, '=', a + b)
