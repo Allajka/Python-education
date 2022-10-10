7747eeb2b38b42187a7f49792cf43ad78eb42367
allajka <allajka94@gmail.com>
2022-10-10 16:52:15 +0300

    homework005

--- homework/homework005/4_exercise.py
+++ homework/homework005/4_exercise.py
@@ -8,35 +8,31 @@ def coding_information_list(row):
             count += 1
             if i == len(row) - 2:
                 count += 1
-                coding_list.append(count)
+                coding_list.append(str(count))
                 coding_list.append(row[i])
                 count = 0
         else:
             count += 1
-            coding_list.append(count)
+            coding_list.append(str(count))
             coding_list.append(row[i])
             count = 0
     return coding_list
 
+def recovery(row):
+    decode = ''
+    count = ''
+    for i in row:
+        if i.isdigit():
+            count += i
+        else:
+            decode += i * int(count)
+            count = ''
+    return decode
 
-def coding_information_string(coding_list):
-    coding_string = ''
-    for i in range(len(coding_list)):
-        coding_string = coding_string + str(coding_list[i])
-    return coding_string
-
-
-def data_recovery(coding_list):
-    string_recovery = ''
-    for i in range(0, len(coding_list), 2):
-        string_recovery = string_recovery + (coding_list[i + 1] * coding_list[i])
-    return string_recovery
-
-
-string = "WWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"
+string = "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwffrtg"
 coding_list = coding_information_list(string)
-coding_string = coding_information_string(coding_list)
-recovery = data_recovery(coding_list)
+coding_string = ''.join(coding_list)
+recovery = recovery(coding_string)
 
 print('RLE алгоритм: модуль сжатия и восстановления данных.\n')
 print(f'Исходные данные: {string}')
