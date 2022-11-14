5dadb606486b0949605ff7826d0c6972b64d13c9
Allajka <allajka94@gmail.com>
2022-11-14 23:52:18 +0300

    homework011

--- lectures/exam006/sympy.py
+++ lectures/exam006/sympy.py
@@ -2,12 +2,12 @@ from sympy import *
 from sympy.plotting import plot
 init_printing()
 
-x = Symbol('x')
-f = x + 2022
-f.subs(x, -77)
-f.subs(x,-210)
-plot(f)
-print(f)
-g = sin(x+x)/x
-plot(g)
-print(g)
+# x = Symbol('x')
+# f = x + 2022
+# f.subs(x, -77)
+# f.subs(x,-210)
+# plot(f)
+# print(f)
+# g = sin(x+x)/x
+# plot(g)
+# print(g)
